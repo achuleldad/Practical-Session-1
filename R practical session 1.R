ae635ec542ef95e0bfc9a94623dae154b4e2f5c9
unknown <l00179049@atu.ie>
2024-03-12 14:01:07 +0000

    didnt overwrite values of x and y

--- R practical session 1.R
+++ R practical session 1.R
@@ -4,21 +4,21 @@ y<-c(4,9,11)
 y
 z<- x+y
 z
-x<-x+2
-x
-y<-y+50
-y
-sum(x)
-mean(y)
+x1<-x+2
+x1
+y1<-y+50
+y1
+sum(x1)
+mean(y1)
 w<-c(2,7,NA,20)
 w
 sum(w)
 v<- sum(w,na.rm = TRUE)
 v
 typeof(v)
-x>y
-x<-append(x,12)
-x
+x1>y1
+x1<-append(x1,12)
+x1
 u<-c(5:20)
 u
 u<-append(u,letters[6:10])
